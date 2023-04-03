21769564f0e58819575ace32e65a955603a47268
Facundo Sapienza <fsapienza@berkeley.edu>
2023-04-03 18:57:21 +0000

    update estimation with the params API

--- smpsite/smpsite/estimate.py
+++ smpsite/smpsite/estimate.py
@@ -37,41 +37,22 @@ def estimate_pole(df_sample, params, ignore_outliers=False, method='mean-of-mean
                                                                       'n_samples': len(row.vgp_dec.values)}))
     
     # Now we need to move this to (lat, lon) space. 
-    vgp_lon, vgp_lat, _, _ = pmag.dia_vgp(df_site.vgp_dec, 
-                                          df_site.vgp_inc, 
-                                          0, 
-                                          params.site_lat, 
-                                          params.site_lon)
+    vgp_long, vgp_lat, _, _ = pmag.dia_vgp(df_site.vgp_dec, 
+                                           df_site.vgp_inc, 
+                                           0, 
+                                           params.site_lat, 
+                                           params.site_long)
  
-    df_site["vgp_lon"] = vgp_lon
-    df_site["vgp_lat"] = vgp_lat
+    df_site["vgp_long"] = vgp_long
+    df_site["vgp_lat"]  = vgp_lat
 
     # Final fisher mean
-    pole_estimate = ipmag.fisher_mean(dec=df_site.vgp_lon.values, 
+    pole_estimate = ipmag.fisher_mean(dec=df_site.vgp_long.values, 
                                       inc=df_site.vgp_lat.values)
  
-    if method=='mean-of-means':
-        return pole_estimate['dec'], pole_estimate['inc'], df_site.n_samples.sum(), df_site.n_samples.unique()           
+
+    return pole_estimate['dec'], pole_estimate['inc'], df_site.n_samples.sum(), df_site.n_samples.unique()           
     
-    
-    # this is nice, we solve this in our papaer by 
-    elif method=='MLE-known-kappa':
-        # we solve iterativelly starting from the estimate based on method='mean-of-means'
-        Mu = np.zeros((K+1,3))
-        # move all to cartesian coordinates 
-        Mu[0,:] = pmag.dir2cart([pole_estimate['dec'], pole_estimate['inc']])
-        # sort df_site
-        df_site = df_site.sort_values(by=['sample_site'])
-        Mu[1:,:] = pmag.dir2cart(df_site[['declination', 'inclination']].values)
-        X = ...
-        
-        decired_tolerance = False
-        while iter_max < 1000 and not decired_tolerance:
-            for site in range(1, K+1):
-                ### Add cartesian coordinates to the data 
-                Mu[site,:] = kappa_1 *  + kappa_0 * Mu[0,:]
-            # Normalize 
-            iter_max += 1
 
             
 def simulate_estimations(params, n_iters=100, ignore_outliers=False, seed=None):
@@ -80,7 +61,7 @@ def simulate_estimations(params, n_iters=100, ignore_outliers=False, seed=None):
     returns a DF with results of n_iters simulated poles.
     '''
     
-    poles = {'plon':[], 'plat':[], 'total_samples':[], 'samples_per_sites':[] }
+    poles = {'plong':[], 'plat':[], 'total_samples':[], 'samples_per_sites':[] }
     
     if seed is not None:
         np.random.seed(seed)
@@ -90,9 +71,9 @@ def simulate_estimations(params, n_iters=100, ignore_outliers=False, seed=None):
         df_sample = generate_samples(params)
         
         # estimate_pole() first groups samples by # of site and then computes a fisher mean for the pole (means of means)
-        pole_lon, pole_lat, total_samples, samples_per_site = estimate_pole(df_sample, params, ignore_outliers=ignore_outliers)
+        pole_long, pole_lat, total_samples, samples_per_site = estimate_pole(df_sample, params, ignore_outliers=ignore_outliers)
         
-        poles['plon'].append(pole_lon)
+        poles['plong'].append(pole_long)
         poles['plat'].append(pole_lat)
         poles['total_samples'].append(total_samples)
         poles['samples_per_sites'].append( samples_per_site)
